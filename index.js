afd5835fe412c5c8063680d64021b8d02d66f802
Carl larsson <54720126+G1aucus@users.noreply.github.com>
2024-11-20 14:44:20 +0100

    flyttas

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+
+function dragElement(elmnt) {
+    document.getElementById(elmnt.id).style.zIndex = indexcount;
+    indexcount++;
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // mus pos på start:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    //flytta när den rör på sig
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    //mus pos
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    //nypos
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    //sluta flytta
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
